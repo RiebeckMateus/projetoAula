a993ae64a785c7734cc63f281394dbdb3add0b02
Mateus Riebeck <mateusavs@hotmail.com>
2024-08-31 12:38:18 -0300

    projeto de scrapping com Scrappy

---
+++ app.py
@@ -0,0 +1,27 @@
+import scrapy
+import csv
+
+class Magalu(scrapy.Spider):
+    name = 'm'
+    custom_settings = {
+        'USER_AGENT' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 OPR/112.0.0.0',
+        'FEED_EXPORT_ENCODING': 'utf-8'
+    }
+    
+    def start_requests(self):
+        yield scrapy.Request(f'https://www.magazineluiza.com.br/celulares-e-smartphones/l/te/')
+    
+    def parse(self, response):
+        total = response.xpath('//button[@type="next"]//ancestor::ul//li[position() = last() -1]//text()').get()
+        for i in range(1, int(total) + 1):
+            yield scrapy.Request(f'https://www.magazineluiza.com.br/celulares-e-smartphones/l/te/?page={i}', callback=self.parse2)
+            
+    def parse2(self, response):
+        blocos = response.xpath('//div[@data-testid="product-list"]//li')
+        for bloco in blocos:
+            title = bloco.xpath('.//h2[@data-testid="product-title"]//text()').get()
+            price = bloco.xpath('.//p[@data-testid="price-value"]//text()').get()
+            yield{
+                'title' : title,
+                'price' : price
+            }
